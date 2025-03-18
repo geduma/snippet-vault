5f3e5b00fc0bffef1e724097533cd61f77763e63
geduramc <geduradev@gmail.com>
2025-03-17 23:30:55 -0500

    fix(endpoints): correct GITHUB_REDIRECT_URL by removing duplicate protocol

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,6 +1,6 @@
 export const Endpoints = {
   GET_USER_URL: 'https://api.geduramc.com/snippet-vault/auth',
   GITHUB_AUTH_URL: 'https://github.com/login/oauth/authorize',
-  GITHUB_REDIRECT_URL: 'https://https://snippet.geduramc.com/auth'
+  GITHUB_REDIRECT_URL: 'https://snippet.geduramc.com/auth'
   // GITHUB_REDIRECT_URL: 'http://localhost:5173/auth' 
 }
